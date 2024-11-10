8b217cb86c25a8c9e1be8d9568c7725a698e0026
Brüsk Kurt <hello@brusk.me>
2024-11-11 00:49:01 +0100

    remove s

--- theme.config.tsx
+++ theme.config.tsx
@@ -15,14 +15,5 @@ const config: DocsThemeConfig = {
   },
 }
 
-export default {
-  head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="GoldVault Docs" />
-      <meta property="og:description" content="Explore the documentation of GoldVault App and take more profit of the App." />
-    </>
-  )
-}
 
 export default config
